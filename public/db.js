64253aaba8edf447a949a1d63b541f98f55645d8
Susan Holland <segh@fastmail.com>
2020-07-30 12:49:20 -0400

    checkDatabase function and window event listener to run it

--- public/db.js
+++ public/db.js
@@ -28,3 +28,29 @@ function saveRecord(record) {
 
     pendingStore.add(record);
 }
+
+// Check pendingStore for records and post to database through API, then clear pendingStore
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const pendingStore = transaction.objectStore("pending");
+    const getAll = pendingStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*", "Content-Type": "application/json"
+                }
+            }).then(response => response.json()).then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const pendingStore = transaction.objectStore("pending");
+                pendingStore.clear();
+            });
+        }
+    }
+}
+
+// listen for online status to run checkDatabase
+window.addEventListener("online", checkDatabase);
